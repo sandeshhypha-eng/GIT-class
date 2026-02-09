6c7f5e7cdf55b5b99a0e674b14d65cf5f1c53c8a
Pavithra Shetty <spaviithra6@gmail.com>
2026-02-09 14:50:27 +0000

    Java multiplication and division program

---
+++ calculator.java
@@ -0,0 +1,22 @@
+public class calculator {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        multiply(a, b);
+        divide(a, b);
+    }
+
+    public static void multiply(int x, int y) {
+        System.out.println("Multiplication: " + (x * y));
+    }
+
+    public static void divide(int x, int y) {
+        if (y != 0) {
+            System.out.println("Division: " + (x / y));
+        } else {
+            System.out.println("Division by zero is not allowed");
+        }
+    }
+}
