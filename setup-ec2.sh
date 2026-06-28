a3d451ef456305ce60dd18eca947bb69fc4eb1ad
sandeshhypha-eng <sandeshhypha@gmail.com>
2026-06-28 18:10:13 +0530

    fegf

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -1,2 +1,18 @@
 #bin/sh
 intallhh
+
+
+
+      - name: SonarCloud Scan
+        uses: SonarSource/sonarqube-scan-action@v2
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          SONAR_TOKEN: ${{ secrets.SONAR_TOKEN }}
+        with:
+          projectBaseDir: CalculatorProject
+          args: >
+            -Dsonar.organization=${{ secrets.SONAR_ORGANIZATION }}
+            -Dsonar.projectKey=${{ secrets.SONAR_PROJECT_KEY }}
+            -Dsonar.projectName=${{ secrets.SONAR_PROJECT_NAME }}
+            -Dsonar.host.url=https://sonarcloud.io
+            
